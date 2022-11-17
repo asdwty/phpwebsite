4243161466a72595ef6b84130c9127028dbbe1b0
Yeo Swan Yew <75032868+asdwty@users.noreply.github.com>
2022-11-17 16:36:20 +0800

    Update output.php

--- website/app/output.php
+++ website/app/output.php
@@ -3,27 +3,9 @@
 <head>
 </head>
 <body>
-<h2>Result</h2>
 <?php 
-$check = false;
 $s = $_POST['name'];
-$fh = fopen("a.txt","r")or die("Can't open file");
-while(!feof($fh))
-{
-$line = fgets($fh);
-$a = trim($line);
-if (strcmp($s,$a) == 0)
-{
-    $check = true;
-}
-}
-if ($check == true){
-print ("<h1>" . $s . " is in the list</h1><br>");
-fclose($fh);
-}
-else{ 
-print ("<h1>" . $s . " is not in the list</h1><br>");
-}
+print ("<h1>Search Result for" . $s . "</h1><br>");
 ?>
 <form method="post" action="index.php"> 
   
