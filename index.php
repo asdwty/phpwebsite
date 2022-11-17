b364d0a4214fa4940cab8053e512d079c1393c07
Yeo Swan Yew <75032868+asdwty@users.noreply.github.com>
2022-11-17 15:23:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 </head>
 <body>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+<form method="post" action="output.php"> 
 	Input <input type="text" name="name" value="">
 	<br><br>
   <input type="submit" name="submit" value="Submit">  
