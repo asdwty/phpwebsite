f9f70b9bb4f841ee8345feb4e13ac1c689b1a1fc
Yeo Swan Yew <75032868+asdwty@users.noreply.github.com>
2022-11-17 17:29:56 +0800

    Update output.php

--- website/app/output.php
+++ website/app/output.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
-<head>
-</head>
 <body>
-<?php 
-$s = $_POST['name'];
-print ("<h1>Search Result for" . $s . "</h1><br>");
+
+<?php
+    if($_GET){
+		echo "<h2>You have Search for ";
+        echo $_GET['search'];    
+		echo "</h2>";
+    }
 ?>
+
 <form method="post" action="index.php"> 
   
   <input type="submit" name="submit" value="Back">  
